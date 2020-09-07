781d1ce9746a08be35f7cd501918047a2a0b476d
Sergei22051990 <68336618+Sergei22051990@users.noreply.github.com>
2020-09-07 15:16:19 +0300

    Add files via upload

---
+++ switch_case.js
@@ -0,0 +1,16 @@
+
+var browser = "Chrome";
+
+if (browser == "IE") {
+    alert("О, да у вас IE!");
+}else if (browser == "Chrome") {
+    alert ("Да, эти браузеры мы поддерживаем");
+}else if (browser == "Firefox") {
+    alert ("Да, эти браузеры мы поддерживаем");
+}else if (browser == "Safari") {
+    alert ("Да, эти браузеры мы поддерживаем");
+}else if (browser == "Opera") {
+    alert ("Да, эти браузеры мы поддерживаем");
+}  else {
+    alert("Мы надеемся, что и в вашем браузере все ок!")
+}
